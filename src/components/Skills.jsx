b8dec67a1061a518c07ae6b65685ac1a33375604
mbafousu <carolngue06@gmail.com>
2026-03-17 21:51:05 -0400

    Add Skills section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,23 @@
+export default function Skills() {
+  return (
+    <section id="skills">
+
+      <h2>Technical Skills</h2>
+
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>React Router</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        <li>MongoDB</li>
+        <li>Mongoose</li>
+        <li>REST APIs</li>
+        <li>JWT Authentication</li>
+        <li>Git & GitHub</li>
+        <li>Postman</li>
+      </ul>
+
+    </section>
+  );
+}
