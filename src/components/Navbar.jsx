397b8e23ae77f9cd02dbba9e8d1b8b306492e290
mbafousu <carolngue06@gmail.com>
2026-03-19 17:51:49 -0400

    Add my profile picture and update the css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
 import "./navbar.css";
+import profileImg from "../assets/logo.png";
 
 export default function Navbar() {
   return (
     <header className="navbar">
       <div className="logo-box">
-        <div className="logo-circle">S</div>
+        <img src={profileImg} alt="Suzy profile" className="logo-img" />
         <span className="logo-text">SUZY CAROL MBAFOU NGUEFACK</span>
       </div>
 
       <nav className="nav-links">
         <Link to="/">HOME</Link>
-      <Link to="/about">ABOUT</Link>
-      <Link to="/projects">PROJECTS</Link>
-      <Link to="/skills">SKILLS</Link>
-     <Link to="/contact">CONTACTS</Link>
-       
+        <Link to="/about">ABOUT</Link>
+        <Link to="/projects">PROJECTS</Link>
+        <Link to="/skills">SKILLS</Link>
+        <Link to="/contact">CONTACT</Link>
       </nav>
     </header>
   );
